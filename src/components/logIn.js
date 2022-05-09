5890ecb9863a5d15e532bed7b929e02aa6914411
ii-codeLab <avalosvana@gmail.com>
2022-05-09 18:35:13 -0500

    creación de nodos para la sección de bienvenida dentro de la vista de logIn

--- src/components/logIn.js
+++ src/components/logIn.js
@@ -2,15 +2,25 @@ import { onNavigate } from "../main.js";
 
 export const logIn = () =>{
     const logInsection = document.createElement('section');
-    const nodoH1 = document.createElement('h1');
-    const buttonLogIn = document.createElement('button');
+    const welcomeSection = document.createElement('section');
+    const welcomeLine = document.createElement('p');
+    const appLogo = document.createElement('img');
+    const  appName = document.createElement('h1');
+    const slogan = document.createElement('p');
+    const buttonGoogleLogIn = document.createElement('button');
+
 
     
-    buttonLogIn.textContent = 'logIn';
-    buttonLogIn.addEventListener('click', () =>{
+    buttonGoogleLogIn.textContent = 'logIn';
+    buttonGoogleLogIn.addEventListener('click', () =>{
         onNavigate('/home');
     });
-    nodoH1.textContent = 'This is the "logIn" view'
-    logInsection.append(nodoH1, buttonLogIn);
+    welcomeLine.textContent = 'Welcome to';
+
+    appName.textContent = 'abstarct';
+    slogan.textContent = 'A cool place to improve your english!';
+    logInsection.append(welcomeSection,buttonGoogleLogIn);
+    welcomeSection.append(welcomeLine, appLogo, appName);
+
     return logInsection;
 };
