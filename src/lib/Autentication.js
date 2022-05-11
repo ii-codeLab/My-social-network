1a011d39c8f032a32997be16fcbf124a6d1e35df
ii-codeLab <avalosvana@gmail.com>
2022-05-10 20:57:36 -0500

    incorporación de constantes 'provider', 'auth' y función 'signInWithRedirect()'

--- src/lib/Autentication.js
+++ src/lib/Autentication.js
@@ -1,3 +1,10 @@
-import {getAuth, 
+import { getAuth, 
     GoogleAuthProvider, 
-    signInWithRedirect} from './Firebase.js'; 
+    signInWithRedirect } from './Firebase.js'; 
+import { onNavigate } from '../main.js';
+import instApp from './FirebaseConfiguration.js';
+
+instApp();
+const provider = new GoogleAuthProvider();
+const auth = getAuth();
+signInWithRedirect(auth, provider);
