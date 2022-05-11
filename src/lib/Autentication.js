9013216ceef33981a9a3ee498af3ff556c5d5c18
ii-codeLab <avalosvana@gmail.com>
2022-05-10 21:58:10 -0500

    se insertó función asincrona 'logInGoogle'

--- src/lib/Autentication.js
+++ src/lib/Autentication.js
@@ -1,10 +1,36 @@
 import { getAuth, 
     GoogleAuthProvider, 
-    signInWithRedirect } from './Firebase.js'; 
+    signInWithRedirect,
+    getRedirectResult,
+ } from './Firebase.js'; 
 import { onNavigate } from '../main.js';
 import instApp from './FirebaseConfiguration.js';
 
 instApp();
 const provider = new GoogleAuthProvider();
 const auth = getAuth();
-signInWithRedirect(auth, provider);
+
+export async function loginGoogle(){
+//-------------- Accede con el redireccionamiento a la página de acceso ---------------------------
+    await signInWithRedirect(auth, provider);
+
+//-------------- Recupera el token de OAuth del proveedor de Google ---------------------------
+    getRedirectResult(auth)
+    .then((result) => {
+        // This gives you a Google Access Token. You can use it to access Google APIs.
+        const credential = GoogleAuthProvider.credentialFromResult(result);
+        const token = credential.accessToken;
+
+        // The signed-in user info.
+        const user = result.user;
+    }).catch((error) => {
+        // Handle Errors here.
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // The email of the user's account used.
+        const email = error.email;
+        // The AuthCredential type that was used.
+        const credential = GoogleAuthProvider.credentialFromError(error);
+        // ...
+    });
+}
