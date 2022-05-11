27ec920852348682aa5c2585a798cc91f43cf86e
ii-codeLab <avalosvana@gmail.com>
2022-05-11 00:58:59 -0500

    logIn con google funciona

--- src/components/logIn.js
+++ src/components/logIn.js
@@ -1,6 +1,6 @@
 import { onNavigate } from '../main.js';
-
-
+import { auth, provider} from '../lib/firebaseConfig.js'
+import { signInWithRedirect, getRedirectResult  } from "https://www.gstatic.com/firebasejs/9.7.0/firebase-auth.js"
 
 export const logIn = () =>{
     const logInsection = document.createElement('section');
@@ -24,7 +24,28 @@ export const logIn = () =>{
     
     buttonGoogleLogIn.textContent = 'logIn';
 
-    buttonGoogleLogIn.addEventListener('click', () =>{
+    buttonGoogleLogIn.addEventListener('click', (e) =>{
+        signInWithRedirect(auth, provider);
+        
+        getRedirectResult(auth)
+  .then((result) => {
+    // This gives you a Google Access Token. You can use it to access Google APIs.
+    const credential = GoogleAuthProvider.credentialFromResult(result);
+    const token = credential.accessToken;
+
+    // The signed-in user info.
+    const user = result.user;
+  }).catch((error) => {
+    // Handle Errors here.
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // The email of the user's account used.
+    const email = error.email;
+    // The AuthCredential type that was used.
+    const credential = GoogleAuthProvider.credentialFromError(error);
+    // ...
+  });
+
         onNavigate('/home');
     });
     return logInsection;
---
+++ src/lib/firebaseConfig.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.7.0/firebase-app.js";
+import { getAuth, GoogleAuthProvider, signInWithRedirect, getRedirectResult  } from "https://www.gstatic.com/firebasejs/9.7.0/firebase-auth.js"
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCkRD5a2R7WHSA1xL8rXnvlIUekhpuspqY",
+  authDomain: "abstractsocialnetwork.firebaseapp.com",
+  projectId: "abstractsocialnetwork",
+  storageBucket: "abstractsocialnetwork.appspot.com",
+  messagingSenderId: "420525000671",
+  appId: "1:420525000671:web:dadd1dd4654521253b666a"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const provider = new GoogleAuthProvider(app);
