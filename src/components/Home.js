72cd7728657a4803d7ea1a596329b55a94063ddc
ii-codeLab <avalosvana@gmail.com>
2022-05-15 22:49:51 -0500

    Incorporación de elementos en el header de home

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,12 +3,19 @@ import { auth} from '../lib/firebaseConfig.js';
 import { signOut } from 'https://www.gstatic.com/firebasejs/9.7.0/firebase-auth.js';
 
 export const home = () =>{
+//---------------------- CREACIÓN DE NODOS -----------------------------------------
     const homeSection = document.createElement('section');
-    const nodoH2 = document.createElement('h2');
+    const homeHeader = document.createElement('header');
+    const logoApp = document.createElement('img');
+    const topMessage = document.createElement('h2');    
     const buttonLogOut = document.createElement('button');
-
-    nodoH2.textContent = 'This is the principal view'
+    
+    logoApp.setAttribute('alt', 'logo image');
+    logoApp.setAttribute('src', './images/logoApp.png');
+    topMessage.textContent= 'Time to  imagine and play!';
     buttonLogOut.textContent = 'logout';
+
+//-------------- Incorporación de log Out al botón ----------------------------------
     buttonLogOut.addEventListener('click', (e) =>{
         signOut(auth).then(() => {
         // Sign-out successful.
@@ -17,6 +24,9 @@ export const home = () =>{
         });
         onNavigate('/');
     });
-    homeSection.append(nodoH2, buttonLogOut);
+
+//--------------- Herencia de nodos ---------------------------------------------------
+    homeHeader.append(logoApp, topMessage, buttonLogOut);
+    homeSection.append(homeHeader);
     return homeSection;
 };
